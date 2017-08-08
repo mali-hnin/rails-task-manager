96a3baf872ec36e2690a305124ad8aa44f0375bf
Mariasole Alossa <mariasole.alossa@gmail.com>
2017-08-08 14:15:42 +0100

    tasks controller and views for index, show, view, destroy. started with create, edit and update

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,22 +1,32 @@
 class TasksController < ApplicationController
   def index
+    @tasks = Task.all
   end
 
   def show
+    @task = Task.find(params[:id])
   end
 
   def new
+    @task = Task.new
   end
 
   def create
+    @task = Task.new(params[:restaurant])
+    @task.save
   end
 
   def edit
+    @task = Task.find(params[:id])
   end
 
   def update
+    @task = Task.find(params[:id])
+    @task.update(params[:task])
   end
 
   def destroy
+    @task = Task.find(params[:id])
+    @task = Task.destroy
   end
 end
